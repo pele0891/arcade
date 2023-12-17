8c68c2dbc67d599bc0a85198cb8638e4e691120d
Peter Lee <pele0891@colorado.edu>
2023-12-16 21:21:31 -0700

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public', 'gameIndex.html')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'gameIndex.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
