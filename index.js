199889637202ecd59956e09fa1b151c6d294bcd0
Peter Lee <pele0891@colorado.edu>
2023-12-16 21:15:29 -0700

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'gameIndex.html')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
