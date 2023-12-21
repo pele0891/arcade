9d3fe4284ab26f08df3138148eb823e1345803dd
Peter Lee <pele0891@colorado.edu>
2023-12-20 17:58:16 -0700

    lobby

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const exp = require('constants');
 
 const app = express();
+const server = http.createServer(app);
+
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'UserID.html'))
 } )
-
 app.set('port', port)
-const server = http.createServer(app)
 
 server.listen(port, () => console.log(`server running on port ${port}`))
